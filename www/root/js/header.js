3f91142b22c1ad9238625434b7a6db2457fb714a
Django-Argus <malaska@mailo.com>
2024-05-05 10:57:39 +0200

    Update index taskflow

--- www/root/js/header.js
+++ www/root/js/header.js
@@ -1,12 +1,19 @@
 window.addEventListener("click", (e) => {
+    console.log(document.getElementById("menu-toggle-btn").checked);
+
     if(!isMenu(e.target) && e.target != document.getElementById("menu-toggle-btn")) {
         document.getElementById("menu-toggle-btn").checked = false;
     }
 });
+document.getElementById("header-icon-toggle").onclick = (e) => {
+    document.getElementById("menu-toggle-btn").checked = !document.getElementById("menu-toggle-btn").checked;
+}
 
 function isMenu(target) {
     do {
         if(target.id == "menu")
+            return true;
+        if(target.id == "header-icon-toggle")
             return true;
 
         target = target.parentNode;
