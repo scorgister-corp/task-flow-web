c4da227812f08477c9987089f9f4667b5586f64e
Django-Argus <malaska@mailo.com>
2024-05-28 12:38:46 +0200

    Update signup.js

--- www/root/js/signup.js
+++ www/root/js/signup.js
@@ -15,42 +15,11 @@ function sendForm() {
     if(username != undefined && username != "" && email != undefined && email != "" && password != undefined && password != "" && confirmPassword != undefined && confirmPassword != "" && password === confirmPassword) {
 
         sendPost("/register", {username: username, password: password, email: email}, (success, result) => {
-            if(!success || result["error"] != undefined) {
-                document.getElementById("form-status").innerText = "An error occurred";
+            if(!success || result["code"] != true) {
+                document.getElementById("form-status").innerText = result["message"];
                 return;
             }
-
-            if(result["result"] != 0) {
-                var res = result["result"];
-                var txt = "";
-                switch(res) {
-                    case -1:
-                        txt = "no username";        
-                        break;
-                    case -2:
-                        txt = "no password";        
-                        break;
-                    case -3:
-                        txt = "no email";        
-                        break;
-                    case -4:
-                        txt = "used username";        
-                        break;
-                    case -5:
-                        txt = "used email";        
-                        break;
-                    case -6:
-                        txt = "mail error";        
-                        break;
-
-                    default:
-                        txt = "An error occurred";
-                        break;
-                }
-
-                document.getElementById("form-status").innerText = txt;
-                return;
-            }
+            
             sendPost("/login", {username: username, password: password}, (success, result) => {
                 if(!success || result["error"] != undefined) {
                     document.getElementById("form-status").innerText = "An error occurred";
